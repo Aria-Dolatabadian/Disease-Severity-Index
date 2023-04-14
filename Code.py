4d87a54903dc81534426d0698cd0f66f4b11e9d7
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-04-14 09:36:37 +0800

    Update Code.py

--- Code.py
+++ Code.py
@@ -10,14 +10,23 @@
 
 #DSI = (Σ (class_freq[i] * rating_scores[i]) / (num_plants * max_index)) * 100
 # Get input values from the user
+import matplotlib.pyplot as plt
 num_plants = int(input("Enter the total number of plants: "))  # Enter 50
 max_index = float(input("Enter the maximal disease index: "))  # Enter 5
 rating_scores = list(map(float, input("Enter the score of each rating class, separated by spaces: ").split()))  # Enter 1 2 3 4
-class_freq = list(map(int, input("Enter the frequency of each rating class, separated by spaces: ").split()))  # Enter 10 20 15 5 
+class_freq = list(map(int, input("Enter the frequency of each rating class, separated by spaces: ").split()))  # Enter 10 20 15 5
 # Calculate the Disease Severity Index (DSI)
 numerator = sum([class_freq[i] * rating_scores[i] for i in range(len(class_freq))])  # numerator = (10 * 1) + (20 * 2) + (15 * 3) + (5 * 4) = 115
 denominator = num_plants * max_index  # denominator = 250
 dsi = (numerator / denominator) * 100  # dsi = (115 / 250) * 100 = 46.00
-
 # Print the Disease Severity Index (DSI)
 print("The Disease Severity Index (DSI) is: {:.2f}%".format(dsi))  # This will print: "The Disease Severity Index (DSI) is: 46.00%"
+fig, ax = plt.subplots()
+ax.bar(rating_scores, class_freq)
+ax.set_xlabel("Rating Scores")
+ax.set_ylabel("Frequency")
+ax.set_title("Rating Class Frequency")
+ax.text(0.7, 0.9, f"DSI = {dsi:.2f}%", transform=ax.transAxes, fontsize=14,
+        verticalalignment='top', bbox=dict(boxstyle='round', facecolor='white', alpha=0.5))
+
+plt.show()
